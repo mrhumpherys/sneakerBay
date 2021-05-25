cc64f23c084e018933b7993fdeacba9eb6b2805b
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-24 21:29:40 -0400

    minor changes

--- client/src/components/Bar/index.js
+++ client/src/components/Bar/index.js
@@ -3,46 +3,46 @@ import React from 'react';
 function Bar() {
 
 
-    return(
-        <section class="marketing-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3">
-                        <div class="market-box text-center">
-                            <i class="fas fa-shipping-fast"></i>
-                           <h3>FAST SHIPPING</h3>
-                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                           
+    return (
+        <section className="marketing-bar">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3">
+                        <div className="market-box text-center">
+                            <i className="fas fa-shipping-fast"></i>
+                            <h3>FAST SHIPPING</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
                         </div>
                     </div>
-                    <div class="col-lg-3">
-                       <div class="market-box text-center">
-                        <i class="fas fa-check-double"></i>
-                          <h3>100% AUTHENTIC</h3>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                          
-                       </div>
-                   </div>
-                   <div class="col-lg-3">
-                       <div class="market-box text-center">
-                        <i class="far fa-credit-card"></i>
-                          <h3>EZ PAYMENT</h3>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                          
-                       </div>
-                   </div>
-                   <div class="col-lg-3">
-                       <div class="market-box text-center">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                          <h3>TOP QUALITY</h3>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                          
-                       </div>
-                   </div>
+                    <div className="col-lg-3">
+                        <div className="market-box text-center">
+                            <i className="fas fa-check-double"></i>
+                            <h3>100% AUTHENTIC</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="market-box text-center">
+                            <i className="far fa-credit-card"></i>
+                            <h3>EZ PAYMENT</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="market-box text-center">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <h3>TOP QUALITY</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,14 +4,14 @@ function Hero() {
 
 
     return(
-        <section class="hero d-flex align-items-center">
-            <div class="container">
-                <div class="row d-flex justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="intro text-center">
+        <section className="hero d-flex align-items-center">
+            <div className="container">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-lg-8">
+                        <div className="intro text-center">
                             <h1>Lorem ipsum dolor sit</h1>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab eos quaerat beatae, voluptates cumque labore officiis eveniet reprehenderit aperiam commodi accusamus sapiente non. Harum rem aut iste ab doloribus delectus.</p>
-                            <a href="#" class="btn btn-outline-light btn-lg mt-3">START SEARCHING NOW</a>
+                            <a href="#" className="btn btn-outline-light btn-lg mt-3">START SEARCHING NOW</a>
                         </div>
                     </div>
                 </div>
