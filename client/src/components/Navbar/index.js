2aa3dcc986c90e0e6701688dd9dd4e1165bb724d
Jmax <jmax407@gmail.com>
2021-05-26 19:26:38 -0400

    navbar update

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -11,54 +11,40 @@ function Navbar() {
 
   return (
     <Route>
-      <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <img src="./img/logo.svg" alt="" width="300" height="35"></img>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="text-end">
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav mb-2 mb-lg-0">
-                
-                  {Auth.loggedIn() ? (
-                    <><li className="nav-item">
-                      <Link className="nav-link" aria-current="/" to="/">
-                      <i className="fas fa-home"></i> Home
-                      </Link>
-                      </li>
-                      <li className="nav-item"><Link to="/" onClick={logout}>Logout</Link></li>
-                      
-                    </>
-                  ) : (
-                    <>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/login"><i className="fas fa-user-plus"></i> Login</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/signup"><i className="fas fa-user-plus"></i> Signup</Link>
-                      </li>
-                    </>
-                  )}
 
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container">
+    <Link className="navbar-brand" to="/"><img src="./img/logo.svg" alt="" width="300" height="35"></img></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      
+        {Auth.loggedIn() ? (
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+          <Link className="nav-link" aria-current="page" to="/"><i className="fas fa-home"></i> Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="#" onClick={logout}>Logout</Link>
+        </li>
+        </ul>
+        ) : (
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login"><i className="fas fa-user-plus"></i> Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup"><i className="fas fa-user-plus"></i> Signup</Link>
+            </li>
+          </ul>
+        )}
+        
+
+    </div>
+  </div>
+</nav>
+
     </Route>
   );
 }
