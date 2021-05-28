e50db402ef99606ecf26b536f2accbbb3336117b
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-28 17:30:22 -0400

    added modal componets; got it working with the purchase option and the delete and added a delete confirm modal so they have to confirm 2 times so not to accidentally delete a shoe

--- client/src/components/SneakerContainer/index.js
+++ client/src/components/SneakerContainer/index.js
@@ -1,54 +1,65 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/react-hooks';
-import { DELETE_SHOE} from '../../utils/mutations';
+import { DELETE_SHOE } from '../../utils/mutations';
+import { useParams } from 'react-router-dom';
+import ModalDelete from "../ModalDelete";
 
 
 
 
 function SneakerContainer({ toSell }) {
+    // ================ modal start ================== //
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { id: shoeId } = useParams();
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+    // ================ modal end ================== //
     const [removeMyShoe, { error }] = useMutation(DELETE_SHOE)
-    
 
-const deleteShoeHandler = async e => {
-    e.preventDefault();
-    console.log(`Let's delete this sucker!`);
-    console.log(e.target.id)
-    
-    try{
-        await removeMyShoe({
-            variables: { _id: e.target.id }
-        });
-    } catch (e) {
-        console.log(e);
+
+    const deleteShoeHandler = async e => {
+        e.preventDefault();
+        console.log(`Let's delete this sucker!`);
+        console.log(e.target.id)
+
+        try {
+            await removeMyShoe({
+                variables: { _id: e.target.id }
+            });
+        } catch (e) {
+            console.log(e);
+        }
     }
-}
     return (
         <>
-        {error && <span className="alert alert-danger ml-2">Something went wrong...</span>}
-        {toSell.map((shoe) => (
-            <div className="col-lg-4" key={shoe._id}>
-                <div className="product-container mb-4">
-                <div className="product-image">
-                    <Link to={`/single-product/${shoe._id}`}><img src="./img/sneakers.svg" className="img-fluid" alt="" /></Link>
-                </div>
-                <div className="product-detail mt-3 px-4">
-                    <h3 className="product-name"><Link to={`/single-product/${shoe._id}`}>{shoe.name}</Link></h3>
-                    <div className="d-flex justify-content-between">
-                        <span className="size">Size: {shoe.size}</span>
-                        <span className="price"><strong>${shoe.price}</strong></span>
+            {error && <span className="alert alert-danger ml-2">Something went wrong...</span>}
+            {toSell.map((shoe) => (
+                <div className="col-lg-4" key={shoe._id}>
+                    <div className="product-container mb-4">
+                        <div className="product-image">
+                            <Link to={`/single-product/${shoe._id}`}><img src="./img/sneakers.svg" className="img-fluid" alt="" /></Link>
+                        </div>
+                        <div className="product-detail mt-3 px-4">
+                            <h3 className="product-name"><Link to={`/single-product/${shoe._id}`}>{shoe.name}</Link></h3>
+                            <div className="d-flex justify-content-between">
+                                <span className="size">Size: {shoe.size}</span>
+                                <span className="price"><strong>${shoe.price}</strong></span>
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-start px-4 product-btns modal-btn">
+                            <div className="btn-group my-4" role="group" aria-label="Basic example">
+                                <Link to={`/single-product/${shoe._id}`} className="btn d-flex justify-content-center align-items-center"><i className="fas fa-search"></i></Link>
+                                {/* <button type="button" id={shoe._id} className="btn d-flex justify-content-center align-items-center" onClick={deleteShoeHandler}><i className="fas fa-trash"></i></button> */}
+                                <button type="button" id={shoe._id} className="btn d-flex justify-content-center align-items-center" data-toggle="modal" onClick={() => toggleModal()}><i className="fas fa-trash"></i></button>
+                                {isModalOpen && <ModalDelete shoe={shoe} onClose={toggleModal} />}
+                            </div>
+                        </div>
                     </div>
+
                 </div>
-                <div className="d-flex justify-content-start px-4 product-btns">
-                    <div className="btn-group my-4" role="group" aria-label="Basic example">
-                        <Link to={`/single-product/${shoe._id}`} className="btn d-flex justify-content-center align-items-center"><i className="fas fa-search"></i></Link>
-                        <button type="button" id={shoe._id} className="btn d-flex justify-content-center align-items-center" onClick={deleteShoeHandler}><i className="fas fa-trash"></i></button>
-                    </div>
-                </div>
-            </div>
-            
-            </div>
-        ))}
+            ))}
         </>
     )
 }
