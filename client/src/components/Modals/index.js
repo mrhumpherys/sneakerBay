87fefdba49bc730557ef7e44dc2637660d78609d
Jmax <jmax407@gmail.com>
2021-05-30 17:52:32 -0400

    modal updates

--- client/src/components/Modals/index.js
+++ client/src/components/Modals/index.js
@@ -10,13 +10,14 @@ const Modals = ({ onClose }) => {
         <div className="modalBackdrop">
             <div className="modal-dialog">
                 <div className="modal-content modalContainer">
-                    <div className="modal-header">
-                        <h3 className="modal-title"><strong>Submit Successful</strong></h3>
+                    <div className="modal-header d-flex justify-content-end">
                         <button type="button" className="closeModalBtn" onClick={onClose}>&times;</button>
                     </div>
-                    <div className="modal-body">
-                        <h2>Thank for selling with us!!.</h2>
-                        <Link to="/dashboard" className="btn btn-primary">Return to Dashboard</Link> 
+                    <div className="modal-body text-center">
+                        <h2>Your Shoes have been successfully posted</h2>
+                    </div>
+                    <div className="modal-footer d-flex justify-content-center">
+                    <Link to="/dashboard" className="btn btn-primary">Return to Dashboard</Link>
                         <button  id="modal-btn" type="button" className="btn btn-warning" onClick={onClose}>Add another Shoe</button>
                     </div>
                 </div>
